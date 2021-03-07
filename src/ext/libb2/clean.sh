e228a23c712b0046f36527ba70eabee73d00d51f
Jamie Harper <25673452+jmhrpr@users.noreply.github>
2021-03-07 17:27:41 +0000

    Include blake2b files

---
+++ src/ext/libb2/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+make maintainer-clean
+rm -rf Makefile.in\
+       aclocal.m4\
+       config.guess\
+       config.sub\
+       configure\
+       depcomp\
+       install-sh\
+       ltmain.sh\
+       missing\
+       src/Makefile.in\
+       src/config.h.in
+
